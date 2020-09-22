246c6f9b8c50e686c66b3e7393c2417f574f366e
Arnob <sebgatullah.cuet.cse14@gmail.com>
2020-09-22 14:27:29 +0600

    Product , Customer, Category Created

--- database/migrations/2020_09_22_052945_create_products_table.php
+++ database/migrations/2020_09_22_052945_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_name');
-            $table->integer('SKU_id');
+            $table->string('SKU_id');
             $table->string('qty');
             $table->double('price');
             $table->foreignId('category_id');
